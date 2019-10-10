8e686b7faf096f316f0389ef5e32ad1816c8054d
bgarcia95 <bgarcia.95@outlook.com>
2019-10-10 17:36:12 -0600

    Added all buttons with their own functionality. App finished.

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -12,34 +12,64 @@ class _ContadorPageState extends State<ContadorPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Contador'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Número de taps: ',
-              style: _estiloTexto,
-            ),
-            Text(
-              '$_conteo',
-              style: _estiloTexto,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Contador'),
+          centerTitle: true,
         ),
-      ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _conteo++;
-          });
-        },
-        child: Icon(Icons.add),
-      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Número de taps: ',
+                style: _estiloTexto,
+              ),
+              Text(
+                '$_conteo',
+                style: _estiloTexto,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: _crearBotones());
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30.0),
+        FloatingActionButton(
+          child: Icon(Icons.exposure_zero),
+          onPressed: _reset,
+        ),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(
+          child: Icon(Icons.remove),
+          onPressed: _sustraer,
+        ),
+        SizedBox(width: 5.0),
+        FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _agregar,
+        ),
+      ],
     );
   }
+
+  void _agregar() {
+    setState(() => _conteo++);
+  }
+
+  void _sustraer() {
+    if(_conteo == 0) {
+      return;
+    }
+    setState(() => _conteo--);
+  }
+
+  void _reset() {
+    setState(() => _conteo = 0);
+  }
+  
 }
